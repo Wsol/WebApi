a9d34cc4826143a78ec9aa7b37a2555e65a6075b
jrvboesch <jrvboesch@gmail.com>
2016-03-22 13:49:23 -0600

    home,nav and footer

--- WebSite/assets/js/dev/controllers/home.js
+++ WebSite/assets/js/dev/controllers/home.js
@@ -1,9 +1,21 @@
 "use strict";
 
 app.controller( 'homeCtrl', function( $scope ){
-	
+	var imagepath = 'assets/img/';
 	$scope.data = {
-		home:'Home Page'
+		dashboard: [
+			{
+				'url': 'clases',
+				'desc': 'Listado de clases',
+				'title': 'Clases',
+				'img': imagepath + 'clases.jpg'
+			},
+			{
+				'url': 'perfil?user=1',
+				'desc': 'Perfil de Personas.',
+				'title': 'Perfil',
+				'img': imagepath + 'perfil.jpg'
+			}
+		]
 	}
-	
 } );
--- WebSite/assets/js/prod/controllers/home.js
+++ WebSite/assets/js/prod/controllers/home.js
@@ -1,1 +1,1 @@
-"use strict";app.controller("homeCtrl",function($scope){$scope.data={home:"Home Page"}});
+"use strict";app.controller("homeCtrl",function($scope){var imagepath="assets/img/";$scope.data={dashboard:[{url:"clases",desc:"Listado de clases",title:"Clases",img:imagepath+"clases.jpg"},{url:"perfil?user=1",desc:"Perfil de Personas.",title:"Perfil",img:imagepath+"perfil.jpg"}]}});
